67abcd90e8dfe9079032325f3b21f5c20985fc4e
spark_d <camara.camille@gmail.com>
2022-03-12 19:47:33 +0100

    Debug plugin helmet)

--- app.js
+++ app.js
@@ -33,14 +33,16 @@ mongoose.connect(`mongodb+srv://admin:${MY_APP_SECRET}@hottest0.jnpzq.mongodb.ne
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+app.use(helmet({ 
+  crossOriginResourcePolicy: { policy: "same-site" } 
+}))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-/*app.use(helmet());*/
+
 
 app.use(mongoSanitize());
 
